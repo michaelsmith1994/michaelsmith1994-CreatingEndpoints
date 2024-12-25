33accb4a151e9cdb0747d89477f94bbf7ab02b2d
Michael Smith <michaelsmith1994@yahoo.com>
2024-12-25 19:00:26 +0000

    another lab completed

--- src/main/java/JavalinSingleton.java
+++ src/main/java/JavalinSingleton.java
@@ -17,6 +17,10 @@ public class JavalinSingleton {
     public static Javalin getInstance(){
         Javalin app = Javalin.create();
         
+        app.get("/hello", ctx-> {
+            ctx.result("Hello World");
+        });
+
         //write endpoint here
 
         return app;
